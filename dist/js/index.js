b704e9cc7a6111e90b2b5712b04616d697a0ece1
duyanjun <2055535041@qq.com>
2018-06-25 23:29:38 +0800

    once more

--- dist/js/index.js
+++ dist/js/index.js
@@ -55,6 +55,53 @@
     		$(".gd a").css("background","");
     	}
     })
+    
+      var num = 0;
+      $(".banner_pic li").eq(num).fadeIn();
+			var timer = setInterval(function(){
+				 num++;
+				if(num==$(".banner_pic li").length){
+					num = 0;
+				  }
+				$(".banner_pic li").eq(num).fadeIn().siblings().fadeOut();
+				
+				$(".nav li").eq(num).addClass("select").siblings().removeClass("select");
+				
+			},2000)
+    /****************楼梯的js***********************/
+   
+   $(function(){
+				var flag=true;//避免滚动条事件影响点击事件，声明的一个变量，所以在有滚动条事件的地方为true，而在点击事件时为false。
+				//要想让图片切换就必须用动画改变高度
+				$(window).scroll(function(){
+					if(flag){
+						var scrollTop=$(this).scrollTop();
+						if(scrollTop > 300){
+							$("#floorNav").fadeIn();
+						}else{
+							$("#floorNav").fadeOut();
+						}
+						$("#content li").each(function(){
+							if(scrollTop >= $(this).offset().top - $(this).outerHeight()/2){//进行事件发生的判断
+								var index = $(this).index();
+								$("#floorNav li").eq(index).addClass("hover").siblings().removeClass("hover");
+							}
+						})
+				  	}
+				})
+				$("#floorNav li").click(function(){
+					flag=false;
+					var index=$(this).index();//获取元素所代表的索引。
+					console.log(index);
+					$(this).addClass("hover").siblings().removeClass("hover");
+					$("html,body").animate({"scrollTop":$("#content li").eq(index).offset().top},500,function(){
+						flag = true;
+					})
+				});
+				
+			})
+   
+   
 })
 })(jQuery);
 	
